548320b0e166908a912ae63dab5fb29ac37c4c8f
Joel Lamata <jlamata88@gmail.com>
2026-02-23 16:51:48 +0100

    stats personaje

--- src/app/personaje/personaje.ts
+++ src/app/personaje/personaje.ts
@@ -4,6 +4,15 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { JsonReader } from '../json-reader';
 
+interface stats {
+  strength: number;
+  dexterity: number;
+  constitution: number;
+  intelligence: number;
+  wisdom: number;
+  charisma: number;
+}
+
 @Component({
   selector: 'app-personaje',
   imports: [PRIMENG_IMPORTS],
@@ -11,9 +20,18 @@ import { JsonReader } from '../json-reader';
   styleUrl: './personaje.css',
 })
 export class Personaje implements OnInit {
+  loading = true;
+
   characterName: string = "rym";
   character: any;
-  loading = true;
+  characterStats: stats = {
+    strength: 0,
+    dexterity: 0,
+    constitution: 0,
+    intelligence: 0,
+    wisdom: 0,
+    charisma: 0
+  };
 
   constructor(
     private readonly route: ActivatedRoute,
@@ -26,11 +44,50 @@ export class Personaje implements OnInit {
       const result = await this.jsonReader.getData('characters/' + this.characterName + '.json');
       //console.log(result)
       this.character = result.data;
-    } catch {
-
+      this.loadStats()
+    } catch(error) {
+      console.log(error)
     } finally {
       this.loading = false;
       console.log("loaded")
     }
+  }
+
+  loadStats() {
+    this.characterStats.strength = this.character.stats[0].value;
+    this.characterStats.dexterity = this.character.stats[1].value;
+    this.characterStats.constitution = this.character.stats[2].value;
+    this.characterStats.intelligence = this.character.stats[3].value;
+    this.characterStats.wisdom = this.character.stats[4].value;
+    this.characterStats.charisma = this.character.stats[5].value;
+
+    for(let i in this.character.modifiers.feat) {
+      let bonus = this.character.modifiers.feat[i];
+      switch (bonus.subType) {
+        case 'strength-score':
+          this.characterStats.strength++;
+          break;
+        case 'dexterity-score':
+          this.characterStats.dexterity++;
+          break;
+        case 'constitution-score':
+          this.characterStats.constitution++;
+          break;
+        case 'intelligence-score':
+          this.characterStats.intelligence++;
+          break;
+        case 'wisdom-score':
+          this.characterStats.wisdom++;
+          break;
+        case 'charisma-score':
+          this.characterStats.charisma++;
+          break;
+      }
+    }
+  }
+
+  computeModifier(stat: number) {
+    console.log(stat)
+    return Math.trunc((stat - 10) / 2)
   }
 }
