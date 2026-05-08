7e4b33a86d6ab413d4cd8aae11b87a712498851c
Joel Lamata <jlamata88@gmail.com>
2026-05-08 12:06:38 +0200

    fix procesar entries

--- src/app/services/entry-processor.service.ts
+++ src/app/services/entry-processor.service.ts
@@ -7,15 +7,46 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 export class EntryProcessorService {
   constructor(private sanitizer: DomSanitizer) {}
 
-  processEntry(entry: string): SafeHtml {
-    const processed = entry.replace(/\{@([^ ]+) ([^}]+)\}/g, '<strong>$2</strong>');
-    return this.sanitizer.bypassSecurityTrustHtml(processed);
-  }
-
   processSpellLevel(level: number): string {
     if (level == 0) {
       return 'Cantrip'
     }
     return 'Level ' + level.toString()
+  }
+
+  processEntry(entry: any): SafeHtml {
+    let html = '';
+
+    if (typeof entry === 'string') {
+      html += '<p>' + this.sanitizeHtml(entry) + '</p>';
+    } else if (entry.type === 'list') {
+      html += '<ul>';
+      for (const item of entry.items) {
+        html += '<li>' + this.sanitizeHtml(this.processEntry(item)) + '</li>';
+      }
+      html += '</ul>';
+    } else if (entry.name) {
+      html += '<strong>' + entry.name + '</strong>';
+      if (entry.entries) {
+        for (const childEntry of entry.entries) {
+          html += this.sanitizeHtml(this.processEntry(childEntry));
+        }
+      }
+    } else if (entry.type === 'entries') {
+      if (entry.entries) {
+        for (const childEntry of entry.entries) {
+          html += this.sanitizeHtml(this.processEntry(childEntry));
+        }
+      }
+    }
+
+    return this.sanitizer.bypassSecurityTrustHtml(html);
+  }
+
+  private sanitizeHtml(entry: any): string {
+    if (typeof entry === 'string') {
+      return entry.replace(/\{@([^ ]+) ([^}]+)\}/g, '<strong>$2</strong>');
+    }
+    return '';
   }
 }
