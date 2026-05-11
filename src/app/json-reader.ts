13b9d1d646e7ae540f97d0b661cd45caeaa23460
Joel Lamata <jlamata88@gmail.com>
2026-05-11 09:37:00 +0200

    error url en local

--- src/app/json-reader.ts
+++ src/app/json-reader.ts
@@ -8,16 +8,21 @@ import { lastValueFrom, Observable, of, shareReplay, switchMap, take, tap } from
 export class JsonReader {
   private cache = new Map<string, any>();
 
-  async getData(filename: string): Promise<any> {
-    if (this.cache.has(filename)) {
-      return this.cache.get(filename);
-    }
-
-    const response = await fetch(`./assets/${filename}`);
-    const data = await response.json();
-    this.cache.set(filename, data);
-    return data;
+async getData(filename: string): Promise<any> {
+  if (this.cache.has(filename)) {
+    return this.cache.get(filename);
   }
+
+  const baseUrl = typeof window !== 'undefined'
+    ? window.location.href
+    : 'http://localhost:4200/';
+
+  const url = new URL(`assets/${filename}`, baseUrl).href;
+  const response = await fetch(url);
+  const data = await response.json();
+  this.cache.set(filename, data);
+  return data;
+}
 
   async getPageData(path: string, page: string): Promise<any> {
     const index = await this.getData(path + 'index.json');
