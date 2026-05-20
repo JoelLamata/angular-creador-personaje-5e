71ca110bbef6fd986d273297b2cea501fda85465
Joel Lamata <jlamata88@gmail.com>
2026-05-20 12:28:41 +0200

    temas oscuro y claro yuju

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { MegaMenuModule } from 'primeng/megamenu';
 import { MegaMenuItem } from 'primeng/api';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [MegaMenuModule],
+  imports: [MegaMenuModule, CommonModule],
   templateUrl: './menu.html',
+  styleUrl: './menu.scss',
 })
 export class Menu implements OnInit {
   items: MegaMenuItem[] | undefined;
+  isDarkMode = false;
 
   ngOnInit(): void {
     this.items = [
@@ -32,6 +35,30 @@ export class Menu implements OnInit {
         label: 'Trasfondos',
         routerLink: ['/trasfondos']
       }
-    ]
+    ];
+
+    if (typeof localStorage !== 'undefined') {
+      const savedTheme = localStorage.getItem('theme');
+      if (savedTheme === 'dark') {
+        document.documentElement.classList.add('dark');
+        this.isDarkMode = true;
+      } else {
+        document.documentElement.classList.remove('dark');
+        this.isDarkMode = false;
+      }
+    }
+  }
+
+  toggleTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+    if (typeof localStorage !== 'undefined') {
+      localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+    }
   }
 }
+
