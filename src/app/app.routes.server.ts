773c3953470bba9c463998f219babfcd2601af76
Joel Lamata <jlamata88@gmail.com>
2026-05-23 10:31:12 +0200

    deploy fixes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,7 +3,15 @@ import { routesClases } from './routes-clases';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: ':nombre',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const pages = routesClases;
+      return pages.map((page) => ({ page }));
+    },
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
